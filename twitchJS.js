6315097b4a1aa342aed7499e513fe1ff32aac3b6
Angel Contreras <angelcbenoit@gmail.com>
2017-12-02 22:49:48 -0800

    edit the site design and structured javascript file

---
+++ twitchJS.js
@@ -0,0 +1,26 @@
+/*
+    use: https://wind-bow.gomix.me/twitch-api/streams/:stream
+    to figure out if the user is online
+    if "stream" === null, then it means the user is offline
+    otherwise, "stream" will contain information about the livestream like what games
+    they're playing or how many viewers there are
+    channel.status will give us the title of the stream
+    channel.game will give current game being played
+
+    use: https://wind-bow.gomix.me/twitch-api/users/:user
+    to get image of icon for users who are offline, the image wont be show up in /streams/:stream link
+    if user is offline.
+    "logo" will give us the icon whether user is online of offline
+ */
+
+$(document).ready(function () {
+
+    var urlStream = "https://wind-bow.gomix.me/twitch-api/streams/";
+    var urlLogo = "https://wind-bow.gomix.me/twitch-api/users/";
+
+    
+
+
+
+});
+
