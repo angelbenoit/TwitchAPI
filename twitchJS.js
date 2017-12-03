efdfc3bea94e3f78cca6237bc1f8ca7d4fbc9627
Angel Contreras <angelcbenoit@gmail.com>
2017-12-03 14:13:29 -0800

    edit the site design and structured javascript file

--- twitchJS.js
+++ twitchJS.js
@@ -1,14 +1,14 @@
 /*
-    use: https://wind-bow.gomix.me/twitch-api/streams/:stream
+    use: https://api.twitch.tv/kraken/users/freecodecamp?client_id=epbr8ttvcdj3ox68n97j6q4u20jqyd
     to figure out if the user is online
     if "stream" === null, then it means the user is offline
     otherwise, "stream" will contain information about the livestream like what games
     they're playing or how many viewers there are
     channel.status will give us the title of the stream
     channel.game will give current game being played
-    https://wind-bow.glitch.me/twitch-api/channels/freecodecamp/follows will give us a list of people who user "freecodecamp" is following
+    https://api.twitch.tv/kraken/channels/freecodecamp/follows?client_id=epbr8ttvcdj3ox68n97j6q4u20jqyd will give us a list of people who user "freecodecamp" is following
 
-    use: https://wind-bow.gomix.me/twitch-api/users/:user
+    use: https://api.twitch.tv/kraken/users/freecodecamp?client_id=epbr8ttvcdj3ox68n97j6q4u20jqyd
     to get image of icon for users who are offline, the image wont be show up in /streams/:stream link
     if user is offline.
     "logo" will give us the icon whether user is online of offline
@@ -21,10 +21,10 @@
 
 $(document).ready(function () {
 
-    var urlStream = "https://wind-bow.gomix.me/twitch-api/streams/";
-    var urlLogo = "https://wind-bow.gomix.me/twitch-api/users/";
+    var urlStream = "https://api.twitch.tv/kraken/users/freecodecamp?client_id=epbr8ttvcdj3ox68n97j6q4u20jqyd";
+    var urlLogo = "https://api.twitch.tv/kraken/users/freecodecamp?client_id=epbr8ttvcdj3ox68n97j6q4u20jqyd";
+    var urlFollows = "https://api.twitch.tv/kraken/channels/freecodecamp/follows?client_id=epbr8ttvcdj3ox68n97j6q4u20jqyd";
 
-    
 
 
 
