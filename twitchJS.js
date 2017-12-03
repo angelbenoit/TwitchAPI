de42a29ab8a4186588052a5a3206eb5fd26fe4b0
angelbenoit <angelcbenoit@gmail.com>
2017-12-02 23:49:59 -0800

    Update twitchJS.js

--- twitchJS.js
+++ twitchJS.js
@@ -6,6 +6,7 @@
     they're playing or how many viewers there are
     channel.status will give us the title of the stream
     channel.game will give current game being played
+    https://wind-bow.glitch.me/twitch-api/channels/freecodecamp/follows will give us a list of people who user "freecodecamp" is following
 
     use: https://wind-bow.gomix.me/twitch-api/users/:user
     to get image of icon for users who are offline, the image wont be show up in /streams/:stream link
